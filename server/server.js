41f14ad2141594c80c5a0d1ddf9c802df744a522
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2024-03-10 16:25:27 -0400

    feat: added Sequelize functionality and skeleton code to query the first entry in db

--- server/server.js
+++ server/server.js
@@ -3,10 +3,119 @@
 const express = require("express");
 const cors = require('cors');
 const { Pool } = require('pg');
+const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+
+//const sequelize = new Sequelize(process.env.DB_HOST + "/" + process.env.DB_NAME)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres', 
+    /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    dialectOptions: {
+        // https://stackoverflow.com/questions/25000183/node-js-postgresql-error-no-pg-hba-conf-entry-for-host
+        // See the answer from Victor Fazer.
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // for dev only
+        }
+    },
+});
+
+// const postgres_db = new Sequelize(process.env.DB_HOST);
+async function syncDB() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+syncDB();
+
+// Sequelize table object.
+const database = sequelize.define("sample_data", {
+        "\"(PK) id\"": {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        summary: {
+            type: DataTypes.STRING,
+        },
+        instructions: {
+            type: DataTypes.STRING,
+        },
+        cuisines: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        preparationMinutes: {
+            type: DataTypes.INTEGER,
+        },
+        cookingMinutes: {
+            type: DataTypes.INTEGER,
+        },
+        readyInMinutes: {
+            type: DataTypes.INTEGER,
+        },
+        priceperServing: {
+            type: DataTypes.INTEGER,
+        },
+        dishTypes: {
+            type: DataTypes.STRING,
+        },
+        servings: {
+            type: DataTypes.INTEGER,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+        diet: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        tableName: "sample_data",
+        timestamps: false,
+    },
+)
+
+async function sync_table() {
+    try {
+        await database.sync();
+        console.log("The table for the db has been (re)created.");
+    }
+    catch (error) {
+        console.log("Syncing the table failed for this reason: ", error)
+    }
+};
+sync_table();
+
+async function get_first_ten() {
+    try {
+        const first_ten = await database.findAll({
+            subQuery: false,
+            limit: 10,
+            raw: true,
+        });
+        console.log("attempting to find the first entries in db");
+        console.log(first_ten[0]);
+    }
+    catch(error){
+        console.log("encountered error: ", error)
+    }
+}
+get_first_ten()
 
 // Database connection info
 const pool = new Pool({
