3c7b6b92ed26d79b3a39e9d9b477466a50749d57
philip581 <philipfernandezz@gmail.com>
2024-03-11 16:53:34 -0400

    printing recipes in the client side console

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,23 +3,32 @@ import React, {useEffect, useState} from 'react'
 function Index() {
   
   const [message, setMessage] = useState("Loading")
+  const [recipes,setRecipes] = useState([]);
 
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/")
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
-      response => response.json()
-    ).then(
+    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/").then(
+      response => response.json()).then(
       data => {
-        console.log(data)
-        setMessage(data.message)
+        console.log(data.message);
       }
     )
   }, [])
 
   return (
     <div>
-      <div>Return message from server</div>
-      <div>{message}</div>
+      {/* <div>Return message from server</div>
+      <div>{message}</div> */}
+      <h1>
+        Home
+      </h1>
+      {recipes.map((recipe)=>{
+        <div>
+          <p>{recipe.title}</p>
+          <p>{recipe.diet}</p>
+          <img src={recipe.img}></img>
+        </div>
+      })}
     </div>
   )
 }
