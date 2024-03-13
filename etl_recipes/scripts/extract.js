b716831681c2d1c2995e2529abb8d454e60441ee
VANESSA LOUIS <elm.vanessa.louis@gmail.com>
2024-03-13 18:37:03 -0400

    ETL extraction

--- etl_recipes/scripts/extract.js
+++ etl_recipes/scripts/extract.js
@@ -6,6 +6,8 @@ async function fetchRecipesFromSource(apiEndpoint) {
         const response = await axios.get(apiEndpoint, {
             headers: { 'Authorization': `Bearer ${process.env.SPOON_RECIPES_API_KEY}` }
         });
+        console.log("Data fetched successfully:");
+        console.log(response.data);
         return response.data; // Assuming the API returns the recipe data in response.data
     } catch (error) {
         console.error('Error fetching data:', error);
