f74303572adab3770d01ff7b9cb3e0cfc22d0ca2
philip581 <philipfernandezz@gmail.com>
2024-03-18 01:12:51 -0400

    display data from database to frontend

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,35 @@
-import React, {useEffect, useState} from 'react'
 
-function Index() {
-  
-  const [message, setMessage] = useState("Loading")
+export const getStaticProps = async () =>{
+    const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/");
+    const data = await res.json();
 
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
-  useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
-      response => response.json()
-    ).then(
-      data => {
-        console.log(data)
-        setMessage(data.message)
-      }
-    )
-  }, [])
+    return{
+      props: {recipes: data.recipeData}
+    }
+}
+
+const Index = ({recipes}) =>{
+
+  console.log(recipes);
 
   return (
     <div>
-      <div>Return message from server</div>
-      <div>{message}</div>
+      <h1>All Recipes</h1>
+      <div className="recipe-card">
+      <img src={recipes[0].image}></img>
+        <p className="recipe-title">{recipes[0].title}</p>
+        <p>{recipes[0].summary}</p>
+      </div>
+      <div className="recipe-card">
+      <img src={recipes[3].image}></img>
+        <p className="recipe-title">{recipes[3].title}</p>
+        <p>{recipes[3].summary}</p>
+      </div>
+      <div className="recipe-card">
+      <img src={recipes[2].image}></img>
+        <p className="recipe-title">{recipes[2].title}</p>
+        <p>{recipes[2].summary}</p>
+      </div>
     </div>
   )
 }
