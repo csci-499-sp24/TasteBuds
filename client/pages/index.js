f05c4322d5bc8d11ce14c0b5dac08d0d0d5e7f81
philip581 <philipfernandezz@gmail.com>
2024-03-18 10:04:48 -0400

    frontend styling and start of routes to recipe pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import {kebabCase} from "lodash"
 
 export const getStaticProps = async () =>{
     const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/");
@@ -14,21 +16,29 @@ const Index = ({recipes}) =>{
 
   return (
     <div>
-      <h1>All Recipes</h1>
-      <div className="recipe-card">
-      <img src={recipes[0].image}></img>
-        <p className="recipe-title">{recipes[0].title}</p>
-        <p>{recipes[0].summary}</p>
-      </div>
-      <div className="recipe-card">
-      <img src={recipes[3].image}></img>
-        <p className="recipe-title">{recipes[3].title}</p>
-        <p>{recipes[3].summary}</p>
-      </div>
-      <div className="recipe-card">
-      <img src={recipes[2].image}></img>
-        <p className="recipe-title">{recipes[2].title}</p>
-        <p>{recipes[2].summary}</p>
+      <nav>
+        <Link className="header" href={"/"}>Home</Link>
+      </nav>
+      <p className="featured">Featured Recipes</p>
+      <div className="recipes">
+        <div className="recipe-card">
+        <img className="recipe-img" src={recipes[0].image}></img>
+          <p className="recipe-title">{kebabCase(recipes[0].title)}</p>
+          {/* <p>{recipes[0].summary}</p> */}
+          <Link className="recipe-link" href={`/recipe/${recipes[0].title}`}>Read more...</Link>
+        </div>
+        <div className="recipe-card">
+        <img className="recipe-img" src={recipes[3].image}></img>
+          <p className="recipe-title">{kebabCase(recipes[3].title)}</p>
+          {/* <p>{recipes[3].summary}</p> */}
+          <Link className="recipe-link" href={`/recipe/${recipes[3].title}`}>Read more...</Link>
+        </div>
+        <div className="recipe-card">
+        <img className="recipe-img" src={recipes[2].image}></img>
+          <p className="recipe-title">{kebabCase(recipes[2].title)}</p>
+          {/* <p>{recipes[2].summary}</p> */}
+          <Link className="recipe-link" href={`/recipe/${recipes[2].title}`}>Read more...</Link>
+        </div>
       </div>
     </div>
   )
