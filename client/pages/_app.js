9b053100c8a96db935fcbdbf554f07fbcdda070c
pasteldetective <pi.jackyy@gmail.com>
2024-03-24 19:53:19 -0400

    updated changes to phillip's changes

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import "/styles/index.css";
+import "/styles/search.css";
+import "/styles/login.css";
+import "/styles/login.css";
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
