99809c5edd7679ba52526029a05a990e4b2089dd
Baikang0000 <158111728+Baikang0000@users.noreply.github.com>
2024-03-25 14:04:30 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 //const sequelize = new Sequelize(process.env.DB_HOST + "/" + process.env.DB_NAME)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     database: process.env.DB_NAME,
@@ -187,7 +190,27 @@ app.get("/cuisines_types", (req, res) => {
     })
 });
 
+/*pool.query('Select * from USERS', (err,res)=>{
+    if(!err){
+        console.log(res.rows);
+    }
+    else{
+        console.log(err.message);
+    }
+    pool.end;
+});*/
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/pages/login.js');
+});
+app.post('/login', (req, res) => {
+    let user = req.body.user;
+    let password = req.body.password;
+    res.send('Username: $(user) Password: $(password)');
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
