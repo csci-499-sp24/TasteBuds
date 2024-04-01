6bad0249a33a8f09701b532317e6d7df11fcf6ed
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2024-04-01 05:31:21 -0400

    Add GitHub Actions workflow for Jest coverage report

--- etl_recipes/scripts/extract.js
+++ etl_recipes/scripts/extract.js
@@ -30,24 +30,3 @@ async function fetchRecipesFromSource(apiEndpoint) {
 }
 
 module.exports = { fetchRecipesFromSource };
-
-
-// // Utility function to perform API requests
-// const axios = require('axios');
-
-// async function fetchRecipesFromSource(apiEndpoint) {
-//     try {
-//         const response = await axios.get(apiEndpoint, {
-//             headers: { 'Authorization': `Bearer ${process.env.SPOON_RECIPES_API_KEY}` }
-//         });
-//         console.log("Data fetched successfully:");
-//         console.log(response.data);
-//         return response.data; // Assuming the API returns the recipe data in response.data
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         return null;
-//     }
-// }
-
-// module.exports = { fetchRecipesFromSource };
-
