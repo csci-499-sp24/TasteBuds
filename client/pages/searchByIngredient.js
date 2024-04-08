7f976200a5c9d8beaee5deb965286d5a29dc0a2e
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2024-04-08 05:02:25 -0400

    fixed formating

--- client/pages/searchByIngredient.js
+++ client/pages/searchByIngredient.js
@@ -105,9 +105,10 @@ function SearchByIngredient() {
       <div className="sidebar">
         <header>TasteBuds</header>
         <ul>
-          <li><Link href="/"><i className="fas fa-home"></i>Home</Link></li> 
-          <li><Link href="/search"><i className="fas fa-search"></i>Search</Link></li>  
-          <li><Link href="/searchByIngredients"><i className="fas fa-search"></i>Search By Ingredients</Link></li>
+          <li><Link href="/"><i className="fas fa-home"></i>Home</Link></li>  
+          <li><Link href="/search"><i className="fas fa-search"></i>Search</Link></li> 
+          <li><Link href="/discover"><i className="fas fa-search"></i>Discover</Link></li> 
+          <li><Link href="/searchByIngredient"><i className="fas fa-search"></i>Ingredient Search</Link></li> 
           <li><a href="#"><i className="fas fa-star"></i>Saved Recipes</a></li>
           <li><a href="#"><i className="fas fa-cog"></i>User Settings</a></li>
           <li><Link href="/login"><i className="fas fa-sign-in-alt"></i>Login</Link></li>
@@ -116,7 +117,7 @@ function SearchByIngredient() {
       <section>
           <div id="div-center" className="">
             <label htmlFor="search">Search Recipes</label>
-            <Autocomplete
+            <Autocomplete 
               label="Select an Ingredient"
               onSelectionChange={handleIngredientSelect}
             >
