a378cd04754a91c2e0b29e39138fded64bcc277f
Kevry Paulino <kevryp@gmail.com>
2024-04-07 20:55:40 -0400

    post merge fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,11 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-<<<<<<< HEAD
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-=======
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
->>>>>>> main
   ],
   theme: {
     extend: {
