ae1933524c05d80cb2fd884b93b467ea6cbcf6df
AbdulRafay09 <92826752+AbdulRafay09@users.noreply.github.com>
2024-04-13 17:50:49 -0400

    Made UI Changes with Card

--- client/pages/home.js
+++ client/pages/home.js
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Sidebar from "./sidebar";
 
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Button,
+} from "@nextui-org/react";
+
 function Homepage() {
   const [randomRecipe, setRandomRecipe] = useState(null);
-  // const [nextUpdateTimer, setNextUpdateTimer] = useState(86400); // Initial 24 hours in seconds
   const [nextUpdateTimer, setNextUpdateTimer] = useState(60); // Value set to 60 seconds for demo
+  // const [nextUpdateTimer, setNextUpdateTimer] = useState(86400); // Initial 24 hours in seconds
 
   const fetchRandomRecipe = async () => {
     try {
@@ -18,26 +27,21 @@ function Homepage() {
         setRandomRecipe(data);
         localStorage.setItem('randomRecipe', JSON.stringify(data));
         localStorage.setItem('lastFetchTime', Date.now().toString());
-        // Reset the timer for the next update
-        // setNextUpdateTimer(86400); // Reset the timer to 24 hours
         setNextUpdateTimer(60); // Reset the timer to 60 seconds
+        // setNextUpdateTimer(86400); // Reset the timer to 24 hours
       };
 
       const lastFetchTime = localStorage.getItem('lastFetchTime');
-      if (!lastFetchTime || Date.now() - parseInt(lastFetchTime) >= 60 * 1000) { //60sec
+      if (!lastFetchTime || Date.now() - parseInt(lastFetchTime) >= 60 * 1000) {
       // if (!lastFetchTime || Date.now() - parseInt(lastFetchTime) >= 24 * 60 * 60 * 1000) { //24hr
         await getRandomRecipeFromServer();
       } else {
         const recipeFromStorage = localStorage.getItem('randomRecipe');
         setRandomRecipe(JSON.parse(recipeFromStorage));
-        // Calculate remaining time for the timer
-        const remainingTime = 60 * 1000 - (Date.now() - parseInt(lastFetchTime)); //60sec
+        const remainingTime = 60 * 1000 - (Date.now() - parseInt(lastFetchTime));
         // const remainingTime = 24 * 60 * 60 * 1000 - (Date.now() - parseInt(lastFetchTime)); //24hr
-        // Update the timer value
         setNextUpdateTimer(Math.ceil(remainingTime / 1000));
-        // Set a timeout to fetch a new recipe after the remaining time
         const timer = setTimeout(getRandomRecipeFromServer, remainingTime);
-        // Clean up the timers when the component unmounts or when a new recipe is fetched
         return () => clearTimeout(timer);
       }
     } catch (error) {
@@ -50,11 +54,9 @@ function Homepage() {
   }, []);
 
   useEffect(() => {
-    // Set up a countdown timer for the next update
     const countdownTimer = setInterval(() => {
       setNextUpdateTimer((prev) => {
         if (prev === 0) {
-          // Reset the timer and fetch a new random recipe
           fetchRandomRecipe();
           return 60; // Reset the timer to 60 sec
           // return 86400; // Reset the timer to 24 hours
@@ -64,22 +66,8 @@ function Homepage() {
       });
     }, 1000);
 
-    // Clean up the countdown timer when the component unmounts
     return () => clearInterval(countdownTimer);
   }, []);
-
-  const recipeCard = (recipe) => {
-    return (
-      <div className="flex justify-center items-center">
-        <div key={recipe.recipe_id} className="card flex-none w-[1000px] h-[750px] justify-center items-center rounded">
-          <div className="header" style={{ maxWidth: '650px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', color: 'black', textAlign: 'center' }}>
-            {recipe.title}
-          </div>
-          <img src={recipe.image} alt={recipe.title} className="recipe-image rounded" style={{ width: '100%', height: 'auto' }} />
-        </div>
-      </div>
-    );
-  };
 
   return (
     <div>
@@ -88,24 +76,19 @@ function Homepage() {
         <i className="fas fa-bars" id="btn"></i>
         <i className="fas fa-times" id="cancel"></i>
       </label>
-      {/* <div className="sidebar">
-        <header>TasteBuds</header>
-        <ul>
-          <li><Link href="/home"><i className="fas fa-home"></i>Home</Link></li>  
-          <li><Link href="/search"><i className="fas fa-search"></i>Search</Link></li> 
-          <li><Link href="/discover"><i className="fas fa-search"></i>Discover</Link></li> 
-          <li><Link href="/searchByIngredient"><i className="fas fa-search"></i>Ingredient Search</Link></li> 
-          <li><a href="#"><i className="fas fa-star"></i>Saved Recipes</a></li>
-          <li><a href="#"><i className="fas fa-cog"></i>User Settings</a></li>
-          <li><Link href="/login"><i className="fas fa-sign-in-alt"></i>Login</Link></li>
-        </ul>
-      </div> */}
       <Sidebar />
       <section className='bg'>
         <div id="div-center" className="head">
           Recipe of the Day:
-          {randomRecipe && recipeCard(randomRecipe)}
-          <div>Next update in: {nextUpdateTimer} seconds</div>
+          {randomRecipe && (
+            <Card className="recipe-card" style={{ height: '500px', width: '700px', backgroundImage: `url(${randomRecipe.image})`, backgroundSize: 'cover' }}>
+              <CardHeader>
+                <h4 style={{fontWeight: "bolder", color: "white", textShadow: "black 2px 2px"}}>{randomRecipe.title}</h4>
+              </CardHeader>
+              {/* <CardBody>{randomRecipe.summary}</CardBody> */}
+            </Card>
+          )}
+          <div style={{fontWeight: "bolder", color: "white", textShadow: "black 2px 2px"}}>Next update in: {nextUpdateTimer} seconds</div>
         </div>
       </section>
     </div>
