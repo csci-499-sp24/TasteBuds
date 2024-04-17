fb08d4f71ecf9d9cb36c793653794605a8dc1c93
philip581 <philipfernandezz@gmail.com>
2024-04-17 19:13:43 -0400

    fix saved recipes error

--- client/pages/savedRecipes.js
+++ client/pages/savedRecipes.js
@@ -1,24 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../firebase/userAuthContext';
 import { firestore } from '../firebase/firebaseConfig';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, query, getDocs } from 'firebase/firestore';
 
 export default function SavedRecipes() {
   const { currentUser } = useAuth();
   const [savedRecipes, setSavedRecipes] = useState([]);
 
   useEffect(() => {
-    const testRef = firestore.collection('testCollection');
-    testRef.get().then((querySnapshot) => {
-        const recipes = querySnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()  
-        }));
-        setSavedRecipes(recipes);  
-        console.log('Fetched recipes:', recipes);  
-    }).catch((error) => {
-        console.error('Test collection fetch error:', error);
-    });
+    const fetchSavedRecipes = async()=>{
+      const testRef = collection(firestore, "testCollection");
+      const queryDb = query(testRef);
+      const querySnapshot = await getDocs(queryDb);
+      const recipes = querySnapshot.docs.map(doc => doc.data());
+      setSavedRecipes(recipes);
+      console.log(recipes);
+    }
+
+    fetchSavedRecipes();
+
+    // testRef.get().then((querySnapshot) => {
+    //     const recipes = querySnapshot.docs.map(doc => ({
+    //       id: doc.id,
+    //       ...doc.data()  
+    //     }));
+    //     setSavedRecipes(recipes);  
+    //     console.log('Fetched recipes:', recipes);  
+    // }).catch((error) => {
+    //     console.error('Test collection fetch error:', error);
+    // });
   }, []);  
 
   return (
