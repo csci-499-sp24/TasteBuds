ae71a5c5d192b4e90a40caa29a9c6911bcc5be98
philip581 <philipfernandezz@gmail.com>
2024-04-24 19:25:31 -0400

    resolve conflicts

--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -17,7 +17,12 @@ function Sidebar() {
         <li><Link href="/discover"><i className="fas fa-search"></i>Discover</Link></li> 
         <li><Link href="/searchByIngredient"><i className="fas fa-search"></i>Ingredient Search</Link></li> 
         {currentUser && ( <>
-        <li><a href="#"><i className="fas fa-star"></i>Saved Recipes</a></li>
+        <li><a href="/savedRecipes"><i className="fas fa-star"></i>Saved Recipes</a></li>
+        <li>
+              <Link href={`/users/${currentUser.uid}`}>
+                <i className="fas fa-user"></i>Profile
+              </Link>
+            </li>
         <li><a href="#"><i className="fas fa-cog"></i>User Settings</a></li>
         <li><a href="#" onClick={handleSignOut}><i className="fas fa-sign-out-alt"></i>Sign Out</a></li>
         </>)}
