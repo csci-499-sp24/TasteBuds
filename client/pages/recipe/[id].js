beb7a0cfc47e102db4140dfd270fa009c29aeeb4
Jackie Yee <pi.jackyy@gmail.com>
2024-05-03 11:19:09 -0400

    fixed issue with some of the ingredient images not having the spoonacular prefix

--- client/pages/recipe/[id].js
+++ client/pages/recipe/[id].js
@@ -66,7 +66,8 @@ const Recipe = () => {
           {ingredients && ingredients.length > 0 ? (
             ingredients.map((ingredient, index) => (
               <li key={index}>
-                <img src={ingredient.image} />
+                <img src={ingredient.image.startsWith('http') ? ingredient.image : `https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`} 
+                alt={ingredient.standard_name} />
                 {ingredient.standard_name}
               </li>
             ))
