f29f8730f133ab112fec58cb4b84dbaa235a5535
jasonzehongwu <jasonzehongwu@gmail.com>
2024-05-13 18:57:50 -0400

    attempt: tried another setup to grab the rating data

--- client/components/starpopup.js
+++ client/components/starpopup.js
@@ -3,30 +3,42 @@ import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDis
 import Stars from "./stars";
 //import id from '../pages/recipe/[id]';
 import { useAuth } from '../firebase/userAuthContext';
+import { auth } from '../firebase/firebaseConfig';
 import { useRouter } from 'next/router';
+import {useState, useCallback} from 'react'
 
 function StarsPopup() {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  // const { currentUser } = useAuth(); //use Firebase auth to get user info (I need the uid)
-  // const router = useRouter(); // Initialize useRouter hook to access router object
-  // const { id } = router.query; // Get the id from the router query
-  // const updateRatings = async () => {
-  //   try {
-  //     //const token = await currentUser.getIdToken();
-  //     if (currentUser.uid == null) {
-  //       console.log("no user uid given")
-  //       throw new Error('You are not logged in')
-  //     }
-  //     const response = await fetch(
-  //       `${process.env.NEXT_PUBLIC_SERVER_URL}/add_rating?firebase_user_id=${currentUser.uid}recipe_id=${id}&rating=${rating}`
-  //     );
-  //     if (!response.ok) {
-  //       throw new Error('Failed to update rating info');
-  //     }
-  //   } catch (error) {
-  //     console.error("Error updating rating info:", error);
-  //   }
-  // }
+  const [rating, setRating] = useState(0)
+  const getRating = (my_rating) => {
+    setRating(my_rating)
+  }
+  //const { currentUser } = useAuth(); //use Firebase auth to get user info (I need the uid)
+  const router = useRouter(); // Initialize useRouter hook to access router object
+  const { recipe_id } = router.query; // Get the id from the router query
+  const firebaseUserId = auth.currentUser ? auth.currentUser.uid : null;
+  const updateRatings = async () => {
+    try {
+      //const token = await currentUser.getIdToken();
+      if (firebaseUserId == null) {
+        console.log("no user uid given")
+        throw new Error('You are not logged in')
+      }
+      if (recipe_id == null) {
+        console.log("no recipe uid given")
+        throw new Error('no Recipe ID found')
+      }
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/add_rating?firebase_user_id=${firebaseUserId}recipe_id=${recipe_id}&rating=${rating}`,
+        {method: "POST"}
+      );
+      if (!response.ok) {
+        throw new Error('Failed to update rating info');
+      }
+    } catch (error) {
+      console.error("Error updating rating info:", error);
+    }
+  }
 
   return (
     <>
@@ -37,13 +49,13 @@ function StarsPopup() {
             <>
               <ModalHeader className="flex flex-col gap-1">Give Your Rating</ModalHeader>
               <ModalBody>
-                <Stars />
+                <Stars getRating={useCallback(() => getRating(), [setRating])}/> {/* passing a function to grab the rating value from child */}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
                 </Button>
-                <Button color="primary" onPress={onClose}>
+                <Button color="primary" onPress={() => {updateRatings(); onClose();}}>
                   Submit
                 </Button>
               </ModalFooter>
--- client/components/stars.js
+++ client/components/stars.js
@@ -3,31 +3,31 @@ import { useAuth } from '../firebase/userAuthContext';
 import { useRouter } from 'next/router';
 //import id from '../pages/recipe/[id]';
 
-function Stars(){
+function Stars(){ //getRating is passed from parent and gets the rating value
     const [rating, setRating] = useState(null)//rating is updated upon clicking in the stars popup window, so it should work?
     const [hover, setHover] = useState(null)
     const router = useRouter();
-    const { recipe_id } = router.query; //meant to get recipe id from the link; it should work?
-    const { currentUser } = useAuth(); //use Firebase auth to get user info (I need the uid to pass as firebase user id)
-    //This function is meant to update the ratings table with a new rating.
-    //Not sure if it is even supposed to be in here or another js file.
-    const updateRatings = async () => {
-      try {
-        //const token = await currentUser.getIdToken();
-        if (currentUser.uid == null) {
-          console.log("no user uid given")
-          throw new Error('You are not logged in')
-        }
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_SERVER_URL}/add_rating?firebase_user_id=${currentUser.uid}recipe_id=${recipe_id}&rating=${rating}`
-        );
-        if (!response.ok) {
-          throw new Error('Failed to update rating info');
-        }
-      } catch (error) {
-        console.error("Error updating rating info:", error);
-      }
-    }
+    // const { recipe_id } = router.query; //meant to get recipe id from the link; it should work?
+    // const { currentUser } = useAuth(); //use Firebase auth to get user info (I need the uid to pass as firebase user id)
+    // //This function is meant to update the ratings table with a new rating.
+    // //Not sure if it is even supposed to be in here or another js file.
+    // const updateRatings = async () => {
+    //   try {
+    //     //const token = await currentUser.getIdToken();
+    //     if (currentUser.uid == null) {
+    //       console.log("no user uid given")
+    //       throw new Error('You are not logged in')
+    //     }
+    //     const response = await fetch(
+    //       `${process.env.NEXT_PUBLIC_SERVER_URL}/add_rating?firebase_user_id=${currentUser.uid}recipe_id=${recipe_id}&rating=${rating}`
+    //     );
+    //     if (!response.ok) {
+    //       throw new Error('Failed to update rating info');
+    //     }
+    //   } catch (error) {
+    //     console.error("Error updating rating info:", error);
+    //   }
+    // }
     
     return (
         <div>
@@ -42,6 +42,7 @@ function Stars(){
                   value={currentRating}
                   onChange={() => {
                     setRating(currentRating);
+                    //getRating(currentRating);
                     //updateRatings()
                     //Commented out so it doesn't break existing code
                     //Tested it when uncommented it, it doesn't seem to do anything
