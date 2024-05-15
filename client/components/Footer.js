6e907b127ee6adcd89f6d707aebb4ec5862f87c7
Baikang0000 <bfeng4290@gmail.com>
2024-05-15 16:34:35 -0400

    Updated footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -6,36 +6,6 @@ function Footer(){
     return (
         <div id="section_footer">
           <div className='text-center py-4'>
-            <Link href="/AboutUs" className='btn btn-link'>
-              About Us
-            </Link>
-            <span> | </span>
-            <Link href="/ContactUs" className='btn btn-link'>
-              Contact Us
-            </Link>
-            <span> | </span>
-            <Link href="/FAQ" className='btn btn-link'>
-              FAQ
-            </Link>
-            <br></br>
-            <Link href="#" className='btn btn-link'>
-              <button>
-                <BsTwitterX />
-              </button>
-            </Link>
-            <span>      </span>
-            <Link href="#" className='btn btn-link'>
-              <button>
-                <BsInstagram />
-              </button>
-            </Link>
-            <span>      </span>
-            <Link href="#" className='btn btn-link'>
-              <button>
-                <BsFacebook/>
-              </button>
-            </Link>
-            <span> </span>
             <Link href="https://github.com/csci-499-sp24/TasteBuds" className='btn btn-link'>
               <button>
                 <BsGithub />
